ea08c13f8707c5b87bd78b33bb6efc41900e3320
FoxDaxian <945039036@qq.com>
2018-12-13 19:54:04 +0800

    update: 一个小小的todolist

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,7 @@ class MyApp extends StatelessWidget {
         appBar: new AppBar(
           title: new Text('待办事项'),
         ),
-        body: new Column(
-          children: <Widget>[
-            new Text('待办事项'),
-            new Container(
-              child: new Expanded(
-                child: new Container(
-                  color: Colors.red[50],
-                  child: new TodoList(),
-                ),
-              ),
-            )
-          ],
-        ),
+        body: new TodoList(),
       ),
     );
   }
@@ -43,82 +31,123 @@ class TodoList extends StatefulWidget {
 }
 
 class TodoListState extends State<TodoList> {
+  var todoLists = <Map>[];
+  var todoInput = new TextEditingController(text: '');
+  var todoFocus = new FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new ListView(
-      shrinkWrap: true,
-      padding: const EdgeInsets.all(20.0),
-      children: <Widget>[
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('================'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('================'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('================'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('================'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-        const Text('I\'m dedicating every day to you'),
-        const Text('Domestic life was never quite my style'),
-        const Text('================'),
-        const Text('When you smile, you knock me out, I fall apart'),
-        const Text('And I thought I was so smart'),
-      ],
+    return new Container(
+      margin: EdgeInsets.all(12.0),
+      child: new Flex(
+        direction: Axis.vertical,
+        children: <Widget>[
+          new Container(
+            margin: EdgeInsets.only(top: 20.0),
+            child: new TextField(
+              focusNode: todoFocus,
+              keyboardType: TextInputType.text,
+              controller: todoInput,
+              decoration: new InputDecoration(
+                labelText: '想做的事情',
+                hintText: '这里输入',
+                prefixIcon: new Icon(Icons.access_time),
+              ),
+            ),
+          ),
+          new Container(
+            margin: EdgeInsets.only(
+              right: 20.0,
+              top: 20.0,
+              bottom: 20.0,
+            ),
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                new IconButton(
+                  icon: new Icon(Icons.add),
+                  onPressed: () {
+                    if (todoInput.text != '') {
+                      setState(() {
+                        var m = new Map();
+                        m['value'] = todoInput.text;
+                        m['finised'] = false;
+                        todoLists.insert(0, m);
+                        todoInput.text = '';
+                      });
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          new Container(
+            child: new Expanded(
+              child: new ListView.builder(
+                itemCount: todoLists.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (todoLists[index]['finised'] == false) {
+                          todoLists[index]['finised'] =
+                              !todoLists[index]['finised'];
+                          var temp = todoLists.removeAt(index);
+                          todoLists.insert(todoLists.length, temp);
+                        } else {
+                          todoLists[index]['finised'] =
+                              !todoLists[index]['finised'];
+                          var temp = todoLists.removeAt(index);
+                          todoLists.insert(0, temp);
+                        }
+                      });
+                    },
+                    child: new Container(
+                      color: todoLists[index]['finised'] == true
+                          ? Colors.green[400]
+                          : Colors.red[400],
+                      margin: EdgeInsets.only(
+                        bottom: 20.0,
+                      ),
+                      padding: EdgeInsets.all(8.0),
+                      child: new Wrap(
+                        children: <Widget>[
+                          // 内容部分
+                          new Text(
+                            '${index + 1}: ${todoLists[index]['value']}',
+                            style: new TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          new Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              new IconButton(
+                                color: Colors.white,
+                                icon: new Icon(Icons.delete),
+                                onPressed: () {
+                                  setState(() {
+                                    todoLists.removeAt(index);
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
